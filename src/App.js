9d253c8875c5dee4eb8d533c4c7081b32ccb4e39
dilipmali9211 <dilipmali9211@gmail.com>
2021-09-16 14:02:02 +0530

    React- Redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux'
 
-function App() {
+function App(props) {
+  console.log(props.store)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {props.x}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -22,4 +25,11 @@ function App() {
   );
 }
 
-export default App;
+let mapStateToPropa = (state)=>{
+  return{
+    x:state.noOflaptop
+  }
+}
+let mapDispatchToPropa = ()=>{}
+export default connect(mapStateToPropa,mapDispatchToPropa)(App);
+ 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,66 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+const redux = require('redux')
+const createStore = require('redux').createStore
+
+
+const SELL_LAPTOP = "SELL_LAPTOP"
+const BUY_LAPTOP = "BUY_LAPTOP"
+
+//Action Creator
+
+function buynewlatop() {
+    return {
+        type: BUY_LAPTOP
+    }
+}
+function selloldlaptop() {
+    return {
+        type: SELL_LAPTOP
+    }
+}
+
+//Reducer
+
+let initalstate = {
+    noOflaptop: 10
+}
+
+const rootreducer = (state = initalstate, action) => {
+    switch (action.type) {
+        case BUY_LAPTOP:
+            return {
+                ...state,
+                noOflaptop: state.noOflaptop + 1
+            }
+            break;
+        case SELL_LAPTOP:
+            return {
+                ...state,
+                noOflaptop: state.noOflaptop - 1
+            }
+    }
+}
+
+const mystore = createStore(rootreducer)
+
+
+console.log(mystore)
+
+console.log(mystore.getState())
+
+console.log(mystore.dispatch(buynewlatop()))
+console.log(mystore.dispatch(buynewlatop()))
+console.log(mystore.dispatch(buynewlatop()))
+console.log(mystore.getState())
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={mystore}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
