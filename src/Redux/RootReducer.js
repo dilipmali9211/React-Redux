001a06165e49deb6d990f2b104393d25cdc6aa19
dilipmali9211 <dilipmali9211@gmail.com>
2021-09-18 13:54:13 +0530

    fixed unreachable code

--- src/Redux/RootReducer.js
+++ src/Redux/RootReducer.js
@@ -1,21 +1,19 @@
 
 //Reducer
 
-const { BUY_LAPTOP, SELL_LAPTOP } = require("./Constants");
-
 let initalstate = {
     noOflaptop: 10
 }
 
 export const rootReducer = (state = initalstate, action) => {
     switch (action.type) {
-        case BUY_LAPTOP:
+        case 'BUY_LAPTOP':
             return {
                 ...state,
                 noOflaptop: state.noOflaptop + 1
             }
             break;
-        case SELL_LAPTOP:
+        case 'SELL_LAPTOP':
             return {
                 ...state,
                 noOflaptop:( state.noOflaptop <1)?  state.noOflaptop: state.noOflaptop - 1
